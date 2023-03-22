661572db9873fffa72db7c2bf445ef6302efe70f
IliaKim91 <paterfamelias@gmail.com>
2023-03-22 17:27:07 +0600

    Added Id property

--- NeobisTask/User.cs
+++ NeobisTask/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,9 @@ namespace NeobisTask
 {
     public class User
     {
+        [Required]
+        public int Id { get; set; }
+
         public string Name { get; set; } = null!;
 
         public string LastName { get; set; } = null!;
